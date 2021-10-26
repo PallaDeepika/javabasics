6fa12de3c85f59329e376a9856410b7c1a4be501
Cashify <Cashify@DESKTOP-UH5AKSS>
2021-10-27 00:00:11 +0545

    ThreadSync

--- dxc.java/src/threads/synchronization/ThreadSyncDemo.java
+++ dxc.java/src/threads/synchronization/ThreadSyncDemo.java
@@ -2,11 +2,9 @@ package threads.synchronization;
 
 public class ThreadSyncDemo {
 	public static void main(String[] args) {
-		Table table = new Table(); //common object for both the threads
-		
+		Table table = new Table();		
 		ShamuThread shamuThread = new ShamuThread(table);
 		RamuThread ramuThread = new RamuThread(table);
-		
 		shamuThread.start();
 		ramuThread.start();
 		
