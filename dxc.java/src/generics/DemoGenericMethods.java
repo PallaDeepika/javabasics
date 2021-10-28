582237071977f77d8f37e9c8f13a9f92ec783e2d
Cashify <Cashify@DESKTOP-UH5AKSS>
2021-10-28 13:35:31 +0545

    generics

---
+++ dxc.java/src/generics/DemoGenericMethods.java
@@ -0,0 +1,20 @@
+package generics;
+
+public class DemoGenericMethods {
+	
+	public static <E> void printArray(E[] elements) {
+		for(E element: elements) {
+			System.out.println();
+			}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		Integer[] intArray= {10,20,30,40};
+		Character[] charArray= {'a','b','c','d'};
+		printArray(intArray);
+		printArray(charArray);
+		
+		
+	}
+
+}
