c4552daa4f146c8c3cded4c0b5a762770b1b2c19
Cashify <Cashify@DESKTOP-UH5AKSS>
2021-10-29 14:45:48 +0545

    design/pattern/factory

---
+++ dxc.java/src/design/patterns/factory/Commercialplan.java
@@ -0,0 +1,11 @@
+package design.patterns.factory;
+
+
+	 class  CommercialPlan extends Plan{  
+		   //@override   
+		    public void getRate(){   
+		        rate=7.50;  
+		   }  
+	}
+
+
---
+++ dxc.java/src/design/patterns/factory/Domesticplan.java
@@ -0,0 +1,11 @@
+package design.patterns.factory;
+
+
+	 class  DomesticPlan extends Plan{  
+        //@override  
+         public void getRate(){  
+             rate=3.50;              
+        }  
+   }
+
+
